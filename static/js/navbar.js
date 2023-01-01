5b706ff6787eaae206711e2422e18fb7f0b972fd
Jergus <jergussnahnican@gmail.com>
2023-01-01 10:02:17 +0100

    v-1.0.0

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,4 +1,5 @@
 const menu_btn = document.querySelector('.hamburger');
+const menubar = document.querySelector('.bar');
 const mobile_menu = document.querySelector('.mobile-nav');
 const menu = document.getElementById('mobilemenu')
 const link1 = document.getElementById('link1');
@@ -9,18 +10,18 @@ const link5 = document.getElementById('link5');
 menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
-    if (menu.style.display == "none") {
-        menu.style.display = "none"
+    if (mobile_menu.style.display == "block") {
+        mobile_menu.style.display = "none"
         link1.style.display = "none"
         link2.style.display = "none"
         link3.style.display = "none"
         link4.style.display = "none"
         link5.style.display = "none"
-        console.log("hah")
+
 
     }
     else {
-        menu.style.display = "block"
+        mobile_menu.style.display = "block"
         link1.style.display = "block"
         link2.style.display = "block"
         link3.style.display = "block"
@@ -32,18 +33,18 @@ menu_btn.addEventListener('click', function () {
 menu_btn.addEventListener('dblclick', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
-    if (menu.style.display == "none") {
+    if (mobile_menu.style.display == "block") {
         menu.style.display = "none"
         link1.style.display = "none"
         link2.style.display = "none"
         link3.style.display = "none"
         link4.style.display = "none"
         link5.style.display = "none"
-        console.log("hah")
+
 
     }
     else {
-
+        mobile_menu.style.display = "block"
         link1.style.display = "block"
         link2.style.display = "block"
         link3.style.display = "block"
@@ -52,17 +53,69 @@ menu_btn.addEventListener('dblclick', function () {
     }
 
 })
+// menubar.addEventListener('click', function () {
+//     menu_btn.classList.toggle('is-active');
+//     mobile_menu.classList.toggle('is-active');
+//     console.log('xxxxxx')
+//     if (menu.style.display == "block") {
+//         console.log('none')
+//         menu.style.display = "none"
+//         link1.style.display = "none"
+//         link2.style.display = "none"
+//         link3.style.display = "none"
+//         link4.style.display = "none"
+//         link5.style.display = "none"
+
+
+//     }
+//     else {
+//         console.log('block')
+//         mobile_menu.style.display = "block"
+//         link1.style.display = "block"
+//         link2.style.display = "block"
+//         link3.style.display = "block"
+//         link4.style.display = "block"
+//         link5.style.display = "block"
+//     }
+
+// })
+// menubar.addEventListener('dblclick', function () {
+//     menu_btn.classList.toggle('is-active');
+//     mobile_menu.classList.toggle('is-active');
+//     console.log('xxxxxx')
+//     if (menu.style.display == "block") {
+//         console.log('none')
+//         menu.style.display = "none"
+//         link1.style.display = "none"
+//         link2.style.display = "none"
+//         link3.style.display = "none"
+//         link4.style.display = "none"
+//         link5.style.display = "none"
+
+
+//     }
+//     else {
+//         console.log('block')
+//         menu.style.display = "block"
+//         link1.style.display = "block"
+//         link2.style.display = "block"
+//         link3.style.display = "block"
+//         link4.style.display = "block"
+//         link5.style.display = "block"
+//     }
+
+// })
 
 
 function search() {
     document.querySelector('.m-search').style.left = '50%';
-    console.log('*****')
+    document.querySelector('.m-search').style.display = 'flex';
     document.querySelector('.search-mobile').classList.toggle('is-active');
     document.querySelector('.cancel').classList.toggle('is-active');
 }
 
 function cancel() {
-    document.querySelector('.m-search').style.left = '200%';
+    document.querySelector('.m-search').style.display = 'none';
     document.querySelector('.search-mobile').classList.toggle('is-active');
     document.querySelector('.cancel').classList.toggle('is-active');
     // .style.display = 'none'
@@ -70,13 +123,14 @@ function cancel() {
 
 
 document.addEventListener('click', function (e) {
-    console.log(e.target)
-    if (menu.style.display == "block") {
+
+    if (mobile_menu.style.display == "block") {
         if (mobile_menu.contains(e.target) == false && e.target != menu_btn && e.target != menu) {
             menu_btn.classList.toggle('is-active');
             mobile_menu.classList.toggle('is-active');
+            menu.style.display = "none"
 
-            console.log(e.target)
+
 
         }
     }
