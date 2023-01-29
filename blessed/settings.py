5aa73f9906a27790da9686165d1b00a82698b4a6
Jergus <jergussnahnican@gmail.com>
2023-01-29 20:37:03 +0100

    handler

--- blessed/settings.py
+++ blessed/settings.py
@@ -133,7 +133,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'blessedstore.sk@gmail.com'
-EMAIL_HOST_PASSWORD = 'nwcyhngarzrscbyq'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASS')
 
 # env('EMAIL_HOST_PASS')
 # Static files (CSS, JavaScript, Images)
