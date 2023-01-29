d300a495e619205473323562851b037147c6f7f7
Jergus <jergussnahnican@gmail.com>
2023-01-29 20:32:27 +0100

    handler

--- blessed/settings.py
+++ blessed/settings.py
@@ -133,7 +133,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'blessedstore.sk@gmail.com'
-EMAIL_HOST_PASSWORD = 'nwcyhngarzrscbyq'
+EMAIL_HOST_PASSWORD =env('EMAIL_HOST_PASS')
+
 # env('EMAIL_HOST_PASS')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
