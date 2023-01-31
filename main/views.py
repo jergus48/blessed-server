46aebfa3450b163d0dabe0158cddca3c8dba9d1b
Jergus <jergussnahnican@gmail.com>
2023-01-31 15:50:34 +0100

    handler

--- main/views.py
+++ main/views.py
@@ -17,7 +17,7 @@ from django.conf import settings
 def ProductCharge(request):
     
     if  request.method == 'POST':
-        try:
+        
             print('DATa:',request.POST)
             product=request.POST['name']
             # customer = stripe.Customer.create(email=request.user.email,name=request.user.first_name,description=request.POST['name'],source=request.POST['stripeToken'])
@@ -48,9 +48,7 @@ def ProductCharge(request):
             p.save()
             request.user.products.add(p)
             
-        except:
-            response = redirect('/somethingwentwrong/')
-            return response
+       
     
     return redirect(reverse('succes',args=[product]))
 def WantedCharge(request):
